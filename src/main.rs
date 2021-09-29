db4895ee1e556e10ba1e7dfdc95e73125572293d
Ferronn <ferronn@ferronn.dev>
2021-09-29 20:41:26 +0000

    idiomatic result handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 async fn fetch_text(client: &reqwest::Client, url: String) -> reqwest::Result<String> {
-    return client.get(url).send().await.expect("nope").text().await;
+    client.get(url).send().await?.text().await
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let base = "http://us.patch.battle.net:1119/wow_classic_era";
     let client = reqwest::Client::new();
     let (versions, cdns) = futures::join!(
         fetch_text(&client, format!("{}/versions", base)),
         fetch_text(&client, format!("{}/cdns", base))
     );
-    println!("{}", versions.expect("moo"));
-    println!("{}", cdns.expect("moo"));
+    println!("{}", versions?);
+    println!("{}", cdns?);
+    Ok(())
 }
