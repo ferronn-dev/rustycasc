2d1c21851462312a0fd38f0cfd7beb867788722c
Ferronn <ferronn@ferronn.dev>
2021-10-02 17:05:05 -0500

    use anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use futures::future::FutureExt;
+use anyhow::{anyhow, bail, ensure, Context, Result};
+use bytes::{Buf, BufMut};
 use std::collections::HashMap;
 use std::convert::TryInto;
 
@@ -24,21 +25,14 @@ fn parse_config(s: &str) -> HashMap<&str, &str> {
 }
 
 fn parse_blte(data: &[u8]) -> Result<Vec<u8>> {
-    use bytes::Buf;
     let mut p = data;
-    if &p[0..4] != b"BLTE" {
-        return Result::Err(Error::E("not BLTE format"));
-    }
+    ensure!(&p[0..4] == b"BLTE", "not BLTE format");
     p.advance(4);
     let header_size = p.get_u32();
-    if header_size == 0 {
-        return Result::Err(Error::E("0 header unimplemented"));
-    }
+    ensure!(header_size > 0, "0 header unimplemented");
     let _flags = p.get_u8();
     let chunk_count = (u32::from(p.get_u8()) << 16) | u32::from(p.get_u16());
-    if header_size != chunk_count * 24 + 12 {
-        return Result::Err(Error::E("header size mismatch"));
-    }
+    ensure!(header_size == chunk_count * 24 + 12, "header size mismatch");
     let mut chunkinfo = Vec::<(usize, usize, u128)>::new();
     for _ in 0..chunk_count {
         let compressed_size = p.get_u32().try_into()?;
@@ -49,10 +43,10 @@ fn parse_blte(data: &[u8]) -> Result<Vec<u8>> {
     let mut result = bytes::BytesMut::with_capacity(chunkinfo.iter().map(|x| x.1).sum::<usize>());
     let inflate = miniz_oxide::inflate::decompress_to_vec_zlib_with_limit;
     for (compressed_size, uncompressed_size, checksum) in chunkinfo {
-        if checksum != u128::from_be_bytes(*md5::compute(&p[0..compressed_size])) {
-            return Result::Err(Error::E("chunk checksum error"));
-        }
-        use bytes::BufMut;
+        ensure!(
+            checksum == u128::from_be_bytes(*md5::compute(&p[0..compressed_size])),
+            "chunk checksum error"
+        );
         let encoding_mode = p.get_u8();
         let chunk_data = p.copy_to_bytes(compressed_size - 1);
         let data = match encoding_mode as char {
@@ -60,142 +54,117 @@ fn parse_blte(data: &[u8]) -> Result<Vec<u8>> {
                 println!("{:?}", chunk_data);
                 chunk_data
             }
-            'Z' => bytes::Bytes::from(inflate(data, uncompressed_size)?),
-            _ => return Result::Err(Error::E("invalid encoding")),
+            'Z' => bytes::Bytes::from(
+                inflate(data, uncompressed_size).map_err(|_| anyhow!("deflate error"))?,
+            ),
+            _ => bail!("invalid encoding"),
         };
-        if data.len() != uncompressed_size {
-            return Result::Err(Error::E("invalid uncompressed size"));
-        }
+        ensure!(data.len() == uncompressed_size, "invalid uncompressed size");
         result.put(data)
     }
-    Result::Ok(result.to_vec())
+    Ok(result.to_vec())
 }
-
-#[derive(Clone, Debug)]
-enum Error {
-    E(&'static str),
-}
-impl From<std::io::Error> for Error {
-    fn from(_: std::io::Error) -> Self {
-        Error::E("io error")
-    }
-}
-impl From<reqwest::Error> for Error {
-    fn from(_: reqwest::Error) -> Self {
-        Error::E("http error")
-    }
-}
-impl From<std::str::Utf8Error> for Error {
-    fn from(_: std::str::Utf8Error) -> Self {
-        Error::E("utf8 error")
-    }
-}
-impl From<&'static str> for Error {
-    fn from(s: &'static str) -> Self {
-        Error::E(s)
-    }
-}
-impl From<std::num::TryFromIntError> for Error {
-    fn from(_: std::num::TryFromIntError) -> Self {
-        Error::E("numeric conversion error")
-    }
-}
-impl From<miniz_oxide::inflate::TINFLStatus> for Error {
-    fn from(_: miniz_oxide::inflate::TINFLStatus) -> Self {
-        Error::E("decompression error")
-    }
-}
-type Result<T> = std::result::Result<T, Error>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let patch_base = "http://us.patch.battle.net:1119/wow_classic_era";
     let client = reqwest::Client::new();
-    let fetch = |url| async { Result::Ok(client.get(url).send().await?.bytes().await?) };
+    let fetch = |url| async {
+        let send_ctx = format!("sending request to {}", url);
+        let recv_ctx = format!("receiving content on {}", url);
+        Result::<bytes::Bytes>::Ok(
+            client
+                .get(url)
+                .send()
+                .await
+                .context(send_ctx)?
+                .bytes()
+                .await
+                .context(recv_ctx)?,
+        )
+    };
     let utf8 = std::str::from_utf8;
-    let version = async {
-        let bytes = fetch(format!("{}/versions", patch_base)).await?;
-        let info = utf8(&*bytes)?;
+    let (versions, cdns) = futures::join!(
+        fetch(format!("{}/versions", patch_base)),
+        fetch(format!("{}/cdns", patch_base))
+    );
+    let (versions, cdns) = (versions?, cdns?);
+    let (build_config, cdn_config) = (|| {
+        let info = utf8(&*versions)?;
         let version = parse_info(info)
             .into_iter()
             .find(|m| m["Region"] == "us")
-            .ok_or("missing us version")?;
+            .context("missing us version")?;
         let build = version
             .get("BuildConfig")
-            .ok_or("missing us build config version")?
+            .context("missing us build config version")?
             .to_string();
         let cdn = version
             .get("CDNConfig")
-            .ok_or("missing us cdn config version")?
+            .context("missing us cdn config version")?
             .to_string();
-        Result::Ok((build, cdn))
-    }
-    .shared();
-    let cdn_fetch = async {
-        let bytes = fetch(format!("{}/cdns", patch_base)).await?;
-        let info = utf8(&*bytes)?;
+        Result::<(String, String)>::Ok((build, cdn))
+    })()?;
+    let ref cdn_prefix = (|| {
+        let info = utf8(&*cdns)?;
         let cdn = parse_info(info)
             .into_iter()
             .find(|m| m["Name"] == "us")
-            .ok_or("missing us cdn")?;
+            .context("missing us cdn")?;
         let host = cdn
             .get("Hosts")
-            .ok_or("missing us cdn hosts")?
+            .context("missing us cdn hosts")?
             .split(" ")
             .next()
             .unwrap();
-        let path = cdn.get("Path").ok_or("missing us cdn path")?;
-        let prefix = format!("http://{}/{}", host, path);
-        Result::Ok(move |tag: &'static str, hash: String| async move {
-            let cache_file = format!("cache/{}.{}", tag, hash);
-            let cached = std::fs::read(&cache_file);
-            if cached.is_ok() {
-                return Result::Ok(bytes::Bytes::from(cached.unwrap()));
-            }
-            let url = format!(
-                "{}/{}/{}/{}/{}",
-                prefix,
-                tag,
-                &hash[0..2],
-                &hash[2..4],
-                hash
-            );
-            let data = fetch(url).await?;
-            std::fs::write(&cache_file, &data)?;
-            //assert_eq!(hash, format!("{:x}", md5::compute(&data)), "{}", data.len());
-            Result::Ok(data)
-        })
-    }
-    .shared();
+        let path = cdn.get("Path").context("missing us cdn path")?;
+        Result::<String>::Ok(format!("http://{}/{}", host, path))
+    })()?;
+    let cdn_fetch = |tag: &'static str, hash: String| async move {
+        let cache_file = format!("cache/{}.{}", tag, hash);
+        let cached = std::fs::read(&cache_file);
+        if cached.is_ok() {
+            return Result::<bytes::Bytes>::Ok(bytes::Bytes::from(cached.unwrap()));
+        }
+        let url = format!(
+            "{}/{}/{}/{}/{}",
+            cdn_prefix,
+            tag,
+            &hash[0..2],
+            &hash[2..4],
+            hash
+        );
+        let data = fetch(url).await?;
+        std::fs::write(&cache_file, &data)?;
+        //assert_eq!(hash, format!("{:x}", md5::compute(&data)), "{}", data.len());
+        Ok(data)
+    };
     let buildinfo = async {
-        let (version, cdn_fetch) = futures::join!(version.clone(), cdn_fetch.clone());
-        Result::Ok(
-            parse_config(&utf8(&*cdn_fetch?("config", version?.0).await?)?)
+        Result::<Vec<String>>::Ok(
+            parse_config(&utf8(&(cdn_fetch("config", build_config).await?))?)
                 .get("encoding")
-                .ok_or("missing encoding in buildinfo")?
+                .context("missing encoding in buildinfo")?
                 .split(" ")
                 .map(|x| x.to_string())
-                .collect::<Vec<String>>(),
+                .collect(),
         )
     };
     let cdninfo = async {
-        let (version, cdn_fetch) = futures::join!(version.clone(), cdn_fetch.clone());
-        let _archives = parse_config(&utf8(&*cdn_fetch?("config", version?.1).await?)?)
+        let _archives = parse_config(&utf8(&(cdn_fetch("config", cdn_config).await?))?)
             .get("archives")
-            .ok_or("missing archives in cdninfo")?
+            .context("missing archives in cdninfo")?
             .split(" ")
             .map(|x| x.to_string())
             .collect::<Vec<String>>();
-        Result::Ok(())
+        Result::<()>::Ok(())
     };
     let encoding = async {
-        let (buildinfo, cdn_fetch) = futures::join!(buildinfo, cdn_fetch.clone());
-        let data = cdn_fetch?("data", buildinfo?.remove(1)).await?;
+        let data = cdn_fetch("data", buildinfo.await?.remove(1)).await?;
         parse_blte(&data)
     };
     let _ = cdninfo.await?;
     println!("{}", utf8(&encoding.await?)?);
-    Result::Ok(())
+    Ok(())
 }
 
 #[cfg(test)]
